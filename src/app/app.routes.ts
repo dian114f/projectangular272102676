89ef1e34f51745f4675746ce570e0eafe0c3c098
CILss <mysteryghs92@gmail.com>
2025-10-27 10:49:58 +0700

    update2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { Dashboard } from './dashboard/dashboard';
 import { Login } from './login/login';
 import { Admin } from './admin/admin';
+import { Dashboard2 } from './dashboard2/dashboard2';
+import { Dashboard3 } from './dashboard3/dashboard3';
 
 
 
@@ -11,6 +13,8 @@ export const routes: Routes = [
     { path: "", redirectTo: "login", pathMatch: "full" },
     { path: "admin", component: Admin },
     { path: "dashboard", component: Dashboard },
+    { path: "dashboard2", component: Dashboard2 },
+    { path: "dashboard3", component: Dashboard3 },
     { path: "login", component: Login }
 ];
   
